60e1c8fab61b33bc35796b2d58544b855d1b49fa
Guilherme Albino <guilhermealbino33@gmail.com>
2023-01-20 13:41:56 -0300

    feat: remove express and io from index.js

---
+++ web/config/express.js
@@ -0,0 +1,60 @@
+import { join } from "path";
+import { readFileSync } from "fs";
+import serveStatic from "serve-static";
+import shopify from "../shopify.js";
+import productCreator from "../product-creator.js";
+import GDPRWebhookHandlers from "../gdpr.js";
+
+export default function setupExpress(app, express) {
+  const STATIC_PATH =
+    process.env.NODE_ENV === "production"
+      ? `${process.cwd()}/frontend/dist`
+      : `${process.cwd()}/frontend/`;
+
+  // Set up Shopify authentication and webhook handling
+  app.get(shopify.config.auth.path, shopify.auth.begin());
+  app.get(
+    shopify.config.auth.callbackPath,
+    shopify.auth.callback(),
+    shopify.redirectToShopifyOrAppRoot()
+  );
+  app.post(
+    shopify.config.webhooks.path,
+    shopify.processWebhooks({ webhookHandlers: GDPRWebhookHandlers })
+  );
+
+  // All endpoints after this point will require an active session
+  app.use("/api/*", shopify.validateAuthenticatedSession());
+
+  app.use(express.json());
+
+  app.get("/api/products/count", async (_req, res) => {
+    const countData = await shopify.api.rest.Product.count({
+      session: res.locals.shopify.session,
+    });
+    res.status(200).send(countData);
+  });
+
+  app.get("/api/products/create", async (_req, res) => {
+    let status = 200;
+    let error = null;
+
+    try {
+      await productCreator(res.locals.shopify.session);
+    } catch (e) {
+      console.log(`Failed to process products/create: ${e.message}`);
+      status = 500;
+      error = e.message;
+    }
+    res.status(status).send({ success: status === 200, error });
+  });
+
+  app.use(serveStatic(STATIC_PATH, { index: false }));
+
+  app.use("/*", shopify.ensureInstalledOnShop(), async (_req, res, _next) => {
+    return res
+      .status(200)
+      .set("Content-Type", "text/html")
+      .send(readFileSync(join(STATIC_PATH, "index.html")));
+  });
+}
